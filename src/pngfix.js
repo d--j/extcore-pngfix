3166814ce12d9ec49a0e226a8fd2d6e0529a6035
Daniel Jagszent <daniel@jagszent.de>
2009-11-07 22:32:33 +0100

    * added a new test file * added documentation * changed source to be better compressible * added Ext.CompositeElementLite augmentation so that pngFix() can be called on collections of Ext.Elements (or flyweights of them)

--- src/pngfix.js
+++ src/pngfix.js
@@ -21,8 +21,9 @@ Ext.ns('Ext.ux');
 // define Ext.BLANK_IMAGE_URL for ext core
 Ext.applyIf(Ext,{BLANK_IMAGE_URL:'http:/'+'/extjs.com/s.gif'});
 
-var BGSTYLE = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='{0}',sizingMethod='scale')";
+var BGSTYLE = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='{0}',sizingMethod='scale')", NEEDS_IMAGE_FIX = 'needsImageFix', NEEDS_BACKGROUND_FIX = 'needsBackgroundFix';
 
+// creates an alpha image loader filter for an imagelike element (img, input type=image)
 function fixImagelikeElement(element){
 	var ed = element.dom, eds = ed.style;
 	if (element.parent('a')) {
@@ -31,20 +32,30 @@ function fixImagelikeElement(element){
 	eds.filter = String.format(BGSTYLE, ed.src);
 	ed.src = Ext.BLANK_IMAGE_URL;
 }
+
+// creates an alpha image loader filter for the background of an element
 function fixBackgroundForElement(element) {
 	var eds = element.dom.style, bgurl = element.getStyle('background-image').replace(/^\s*url\(\s*("|')?\s*/,'').replace(/\s*("|')?\s*\)\s*$/,'');
 	eds.backgroundImage = 'none';
 	eds.filter = String.format(BGSTYLE, bgurl);
 }
-function fix(rootElement){
-	Ext.get(rootElement || Ext.getBody()).select('*').each(function(element){
-		if (fix.needsImageFix(element)) {
-			fixImagelikeElement(element);
-		} else if (fix.needsBackgroundFix(element)) {
-			fixBackgroundForElement(element);
-		}
-	});
+
+// checks the element if it needs the PNG fix
+function fixOneElement(element){
+	if (fix[NEEDS_IMAGE_FIX](element)) {
+		fixImagelikeElement(element);
+	} else if (fix[NEEDS_BACKGROUND_FIX](element)) {
+		fixBackgroundForElement(element);
+	}
 }
+
+// checks the root element and every child if it needs one of the two checks and applies them if neccessary
+function fix(rootElement){
+	rootElement = Ext.get(rootElement || Ext.getBody());
+	fixOneElement(rootElement);
+	rootElement.select('*').each(fixOneElement);
+}
+
 /**
  * Gets called for every element and should return true when the element needs the image fix.
  * The image fix only makes sense for img tags but for sake of simplicity needsImageFix gets
@@ -53,35 +64,70 @@ function fix(rootElement){
  * @param {Ext.Element} element   the element to examin
  * @return {Boolean}  true when this element needs to be fixed. false otherwise.
  */
-fix.needsImageFix = function(element){
+fix[NEEDS_IMAGE_FIX] = function(element){
 	var src = element.dom.src;
 	if (src && src.match(/\.png$/)){
 		return true
 	}
 	return false;
 }
+
 /**
  * Gets called for every element and should return true when the element needs the background fix. 
  * @method needsBackgroundFix
  * @param {Ext.Element} element   the element to examin
  * @return {Boolean}  true when this element needs to be fixed. false otherwise.
  */
-fix.needsBackgroundFix = function(element){
+fix[NEEDS_BACKGROUND_FIX] = function(element){
 	var bg = element.getStyle('background-image');
 	if (bg && bg.match(/url\(.*\.png/)) {
 		return true;
 	}
 	return false;
 }
+
+// fake implementation - does nothing
 function fakeFix(){}
-fakeFix.needsImageFix = Ext.emptyFn;
-fakeFix.needsBackgroundFix = Ext.emptyFn;
+fakeFix[NEEDS_IMAGE_FIX] = Ext.emptyFn;
+fakeFix[NEEDS_BACKGROUND_FIX] = Ext.emptyFn;
+
+// public interface - IE6 gets the real thing, all other browsers get a fake implementation
 Ext.ux.PngFix = Ext.isIE6 ? fix : fakeFix;
 })();
 
+/**
+ * @class Ext.Element
+ */
 Ext.Element.addMethods({
+	/**
+	 * An alternate version to call the PNG fix for a single element.
+	 * <br/>
+	 * <strong>Example Usage</strong><br/>
+	 * <code><pre>
+Ext.onReady(function(){Ext.get('needs-pngfix').pngFix();});
+	 * </pre></code>
+	 * @method pngFix
+	 * @return {Ext.Element} the element
+	 */
 	pngFix: function(){
 		Ext.ux.PngFix(this);
 		return this;
 	}
 });
+
+/**
+ * @class Ext.CompositeElementLite
+ */
+/**
+ * An alternate version to call the PNG fix for a collection of elements.
+ * <br/>
+ * <strong>Example Usage</strong><br/>
+ * <code><pre>
+Ext.onReady(function(){Ext.select('.needs-pngfix').pngFix().removeClass('needs-pngfix');});
+ * </pre></code>
+ * @method pngFix
+ * @return {Ext.Element} the element
+ */
+Ext.CompositeElementLite.prototype.pngFix = function(){
+    return this.invoke('pngFix', arguments);
+}
